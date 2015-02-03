598a8b32d72efffe760e2da6b85e1fbccf63ee62
Adam Pere <adam@goinvo.com>
2015-02-03 17:14:11 -0500

    updates from email & image optimization

--- public/js/min/all-min.js
+++ public/js/min/all-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#white-white-red-hero, #white-white-red .project-info").on("click",function(){window.location.href="http://adampere.com/spark/portfolio/white-white-red/"})});
+$(document).ready(function(){$("#white-white-red-hero, #white-white-red .project-info").on("click",function(){window.location.href="http://adampere.com/spark/portfolio/white-white-red/"}),$("#as-i-am-changing-hero, #as-i-am-changing .project-info").on("click",function(){window.location.href="http://adampere.com/spark/portfolio/as-i-am-changing/"})});
