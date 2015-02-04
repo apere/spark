dbda31182906d5e6b744b30b5ef763351d4956be
Adam Pere <adam@goinvo.com>
2015-02-03 19:01:51 -0500

    more archive stuffs & new nav

--- public/js/all.js
+++ public/js/all.js
@@ -1,10 +1,25 @@
 $( document ).ready(function() {
+	//portfolio
     $('#white-white-red-hero, #white-white-red .project-info').on('click', function() {
 		window.location.href = 'http://adampere.com/spark/portfolio/white-white-red/';
 	});
 	
 	$('#as-i-am-changing-hero, #as-i-am-changing .project-info').on('click', function() {
 		window.location.href = 'http://adampere.com/spark/portfolio/as-i-am-changing/';
+	});
+	
+	
+	//archive
+	$('#genesis-dreamtime-hero, #genesis-dreamtime .project-info').on('click', function() {
+		window.location.href = 'http://adampere.com/spark/archive/genesis-and-dreamtime/';
+	});
+	
+	$('#stack-through-hero, #stack-through .project-info').on('click', function() {
+		window.location.href = 'http://adampere.com/spark/archive/stack-through/';
+	});
+	
+	$('#still-hanoi-hero, #still-hanoi .project-info').on('click', function() {
+		window.location.href = 'http://adampere.com/spark/archive/still-hanoi-move/';
 	});
 });
 
