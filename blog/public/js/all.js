a1a64741586aa684cd7c64a206259c4128eb0ca8
adam m pere <adampere001@gmail.com>
2015-07-03 12:12:35 -0400

    blog construction page & blog sketches

---
+++ blog/public/js/all.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+
+    $("img.lazy").lazyload({
+		threshold : 100,
+		effect : "fadeIn"	
+	});
+
+	//portfolio
+    $('#white-white-red-hero, #white-white-red .project-info').on('click', function() {
+		window.location.href = 'http://www.sheripark.com/portfolio/white-white-red/';
+	});
+	
+	$('#as-i-am-changing-hero, #as-i-am-changing .project-info').on('click', function() {
+		window.location.href = 'http://www.sheripark.com/portfolio/as-i-am-changing/';
+	});
+	
+	
+	//archive
+	$('#genesis-dreamtime-hero, #genesis-dreamtime .project-info').on('click', function() {
+		window.location.href = 'http://www.sheripark.com/archive/genesis-and-dreamtime/';
+	});
+	
+	$('#stack-through-hero, #stack-through .project-info').on('click', function() {
+		window.location.href = 'http://www.sheripark.com/archive/stack-through/';
+	});
+	
+	$('#still-hanoi-hero, #still-hanoi .project-info').on('click', function() {
+		window.location.href = 'http://www.sheripark.com/archive/still-hanoi-move/';
+	});
+	
+	$('#today-fri-sat-sun-hero, #today-fri-sat-sun .project-info, #index-hero').on('click', function() {
+		window.location.href = 'http://www.sheripark.com/portfolio/Today-Friday-Saturday-Sunday/index.html';
+	});
+});
+
